3ae4245b0a8edb5939829aedef214c6bda9536d9
Georg Moertl <georgmoertl@aon.at>
2018-11-14 17:02:57 +0100

    add TTs samples

--- preprocessing/step3.sh
+++ preprocessing/step3.sh
@@ -1,22 +1,21 @@
 #!/bin/sh
 
 #PF and SV sorting + isClassId_Training branches
-#python -i step3.py --version v4 --year 2016 --flavour muo --ptSelection pt_15_-1 --sampleSelection DYvsQCD --nJobs 100 --job 0
-#python -i step3.py --version v4 --year 2016 --flavour muo --ptSelection pt_15_-1 --sampleSelection TTJets  --nJobs 50  --job 0
 
-#python -i step3.py --version v4 --year 2016 --flavour muo --ptSelection pt_15_-1 --sampleSelection DYvsQCD #SPLIT20
-#python -i step3.py --version v4 --year 2016 --flavour muo --ptSelection pt_15_-1 --sampleSelection TTJets #SPLIT10
-
-#python -i step3.py --version v5 --year 2016 --flavour muo --ptSelection pt_15_-1 --sampleSelection DYvsQCD #SPLIT20
-#python -i step3.py --version v5 --year 2016 --flavour muo --ptSelection pt_5_15  --sampleSelection TTJets  #SPLIT10
-
-python -i step3.py --version v6 --year 2016 --flavour muo --ptSelection pt_5_-1   --sampleSelection TTJets  #SPLIT20
-python -i step3.py --version v6 --year 2016 --flavour muo --ptSelection pt_5_15   --sampleSelection TTJets  #SPLIT10
-python -i step3.py --version v6 --year 2016 --flavour muo --ptSelection pt_15_25  --sampleSelection TTJets  #SPLIT10
-python -i step3.py --version v6 --year 2016 --flavour muo --ptSelection pt_25_-1  --sampleSelection TTJets  #SPLIT10
+##TTJets
+#python -i step3.py --version v6 --year 2016 --flavour muo --ptSelection pt_5_-1   --sampleSelection TTJets  #SPLIT20
+#python -i step3.py --version v6 --year 2016 --flavour muo --ptSelection pt_5_15   --sampleSelection TTJets  #SPLIT10
+#python -i step3.py --version v6 --year 2016 --flavour muo --ptSelection pt_15_25  --sampleSelection TTJets  #SPLIT10
+#python -i step3.py --version v6 --year 2016 --flavour muo --ptSelection pt_25_-1  --sampleSelection TTJets  #SPLIT10
 
 #python -i step3.py --version v6 --year 2016 --flavour muo --ptSelection pt_5_-1   --sampleSelection TTJets --nJobs 20 --job 0 
 #python -i step3.py --version v6 --year 2016 --flavour muo --ptSelection pt_5_15   --sampleSelection TTJets --nJobs 10 --job 0 
 #python -i step3.py --version v6 --year 2016 --flavour muo --ptSelection pt_15_25  --sampleSelection TTJets --nJobs 10 --job 0 
 #python -i step3.py --version v6 --year 2016 --flavour muo --ptSelection pt_25_-1  --sampleSelection TTJets --nJobs 10 --job 0 
+
+#TTs
+python -i step3.py --version v6 --year 2016 --flavour muo --ptSelection pt_5_-1   --sampleSelection TTs  #SPLIT20
+python -i step3.py --version v6 --year 2016 --flavour muo --ptSelection pt_5_15   --sampleSelection TTs  #SPLIT10
+python -i step3.py --version v6 --year 2016 --flavour muo --ptSelection pt_15_25  --sampleSelection TTs  #SPLIT10
+python -i step3.py --version v6 --year 2016 --flavour muo --ptSelection pt_25_-1  --sampleSelection TTs  #SPLIT10
 

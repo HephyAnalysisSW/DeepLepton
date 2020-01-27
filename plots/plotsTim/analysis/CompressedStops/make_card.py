4f7db930e84df902ed33893a3726a97c14e15fb4
Tim Brueckler <tim.brueckler@gmail.com>
2020-01-27 16:24:02 +0100

    limits

--- plots/plotsTim/analysis/CompressedStops/make_card.py
+++ plots/plotsTim/analysis/CompressedStops/make_card.py
@@ -36,29 +36,35 @@ from DeepLepton.Tools.cutInterpreter    import cutInterpreter
 
 ROOT.gStyle.SetNumberContours(255)
 
+from DeepLepton.Tools.Cache import Cache
 from Analysis.Tools.CardFileWriter  import CardFileWriter
 from regions import regions
 
 baseDir       = os.path.join( cache_directory, "caches" )
 if not os.path.exists( baseDir ): os.makedirs( baseDir )
-
-cacheFileName   = os.path.join( baseDir, "estimates" )
-limitCache      = MergingDirDB( cacheFileName )
-
+estimate_cacheFileName   = os.path.join( baseDir, "estimates" )
+estimateCache      = MergingDirDB( estimate_cacheFileName )
 cardfileLocation = os.path.join( cache_directory, "cardFiles" )
+
+limitDir       = os.path.join( cache_directory, "limits" )
+if not os.path.exists(limitDir): os.makedirs(limitDir)
+limit_cacheFileName = os.path.join(limitDir, 'calculatedLimits')
+limitCache    = Cache(limit_cacheFileName, verbosity=3)
+
 
 sample_list = ["DY", "TTJets_DiLepton", "WJets", "VV", "TTJets_SingleLepton", "SMS"]#, "Data"]
 bg_list = ["DY", "TTJets_DiLepton", "WJets", "VV", "TTJets_SingleLepton"]
 
 
+c = CardFileWriter()
+c.addUncertainty('JES', 'lnN')
+c.addUncertainty('btagging', 'lnN')
+c.addUncertainty('mistagging', 'lnN')
+c.addUncertainty('muonId', 'lnN')
+c.addUncertainty('electronId', 'lnN')
+
 for stopm in [(250+25*i) for i in range(23)]:
     for lspm in [(stopm - delta) for delta in [10,20,30,40,50,60,70,80]]:
-        c = CardFileWriter()
-        c.addUncertainty('JES', 'lnN')
-        c.addUncertainty('btagging', 'lnN')
-        c.addUncertainty('mistagging', 'lnN')
-        c.addUncertainty('muonId', 'lnN')
-        c.addUncertainty('electronId', 'lnN')
         
         observation = []
         for i_region ,region in enumerate(regions):
@@ -69,7 +75,7 @@ for stopm in [(250+25*i) for i in range(23)]:
             nice_name = str(region)
             for bg in bg_list:       
                 key=(bg+'_'+str(region))
-                estimate = limitCache.get(key)
+                estimate = estimateCache.get(key)
                 observation[i_region] += estimate
                 c.specifyExpectation( bin_name, bg, int(estimate) ) 
                 c.specifyUncertainty( 'JES', bin_name, bg, 1.1)
@@ -78,7 +84,7 @@ for stopm in [(250+25*i) for i in range(23)]:
                 c.specifyUncertainty( 'muonId', bin_name, bg, 1.1)
                 c.specifyUncertainty( 'electronId', bin_name, bg, 1.1)
             c.specifyObservation(bin_name, int(observation[i_region]))
-            estimate_sig = limitCache.get("SMS_T2tt_"+str(stopm)+'_'+str(lspm)+'_'+str(region))
+            estimate_sig = estimateCache.get("SMS_T2tt_"+str(stopm)+'_'+str(lspm)+'_'+str(region))
             c.specifyExpectation( bin_name, 'signal', int(estimate_sig))
             c.specifyUncertainty( 'JES', bin_name, 'signal', 1.1)
             c.specifyUncertainty( 'btagging', bin_name, 'signal', 1.1)
@@ -91,9 +97,8 @@ for stopm in [(250+25*i) for i in range(23)]:
         c.writeToFile( cardFilePath )
         print("produced ", cardname)
 
-
-
-
+        limit = c.calcLimit(cardFilePath)
+        limitCache.add((stopm,lspm), limit)
 
 
 

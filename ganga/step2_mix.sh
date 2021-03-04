8686f2b8f32f8b09d05e7b65834529e3259daa71
Dietrich Liko <Dietrich.Liko@oeaw.ac.at>
2021-03-04 19:06:38 +0100

    Add SAMPLES_DBDIR

--- ganga/step2_mix.sh
+++ ganga/step2_mix.sh
@@ -1,11 +1,12 @@
 #! /bin/sh -x 
 #
-# Run step1_select.py on the grid
+# Run step2_mix.py on the grid
 #
 # Dietrich Liko, March 2021
 
 echo "Setting up CMS environment"
 source /cvmfs/cms.cern.ch/cmsset_default.sh
+export SAMPLES_DBDIR=$PWD
 
 echo "Setting up CMS release $CMSSW_VERSION for $SCRAM_ARCH"
 cmsrel $CMSSW_VERSION

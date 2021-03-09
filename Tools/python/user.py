ceb0e151db43745a946456a89011b4ff1ee1106f
Maximilian Moser <max.moser1@gmail.com>
2021-03-09 10:46:05 +0100

    user

--- Tools/python/user.py
+++ Tools/python/user.py
@@ -5,6 +5,13 @@ if os.environ['USER'] in ['robert.schoefbeck']:
     skim_directory   = "/scratch-cbe/users/robert.schoefbeck/DeepLepton/"
     #data_directory   = "/afs/hephy.at/data/rschoefbeck02/cmgTuples/"
 
+if os.environ['USER'] in ['maximilian.moser']:     
+    plot_directory   = "/scratch-cbe/users/maximilian.moser/www/DeepLepton/"
+    #plot_directory   = "~/Plots/"
+    #skim_directory   = "/scratch-cbe/users/maximilian.moser/DeepLepton/"
+    skim_directory   = "/eos/vbc/user/maximilian.moser/DeepLepton"
+    #data_directory   = "/afs/hephy.at/data/mmoser/cmgTuples/"
+
 if os.environ['USER'] in ['gmoertl']:
     plot_directory   = "/afs/hephy.at/user/g/gmoertl/www/"
     skim_directory   = "/afs/hephy.at/data/cms02/DeepLepton/skims/"

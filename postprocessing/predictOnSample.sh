0db911d301896c45ed70bdc4ffe46ff7cfdd5743
Maximilian Moser <max.moser1@gmail.com>
2021-04-19 10:16:26 +0200

    predictOnSample

--- postprocessing/predictOnSample.sh
+++ postprocessing/predictOnSample.sh
@@ -1,1 +1,1 @@
-python predictOnSample.py --small --version v2 --year 2016 --flavour muo --logLevel INFO --ptSelection pt_3.5_-1   --sample DYJetsToLL_M50_LO 
+python predictOnSample.py --small --year 2016 --flavour muo --logLevel INFO --ptSelection pt_3.5_-1   --sample DYJetsToLL_M50_LO 

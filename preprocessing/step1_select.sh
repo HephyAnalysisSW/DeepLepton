37f7bafccf8889d2782aaa9a904cfe6dde00561f
Maximilian Moser <max.moser1@gmail.com>
2021-04-20 12:22:23 +0200

    merge conflict

--- preprocessing/step1_select.sh
+++ preprocessing/step1_select.sh
@@ -1,5 +1,5 @@
-python step1_select.py --small  --version v1 --year 2016 --flavour ele --logLevel INFO --ptSelection pt_5_-1 --sample DYJetsToLL_M50_LO #SPLIT100
-#python step1_select.py  --version v1 --year 2016 --flavour muo --logLevel INFO --ptSelection pt_3.5_-1 --sample DYJetsToLL_M50_LO #SPLIT100
+#python step1_select.py --small  --version v1 --year 2016 --flavour ele --logLevel INFO --ptSelection pt_5_-1 --sample DYJetsToLL_M50_LO #SPLIT100
+python step1_select.py --small --version v1 --year 2016 --flavour muo --logLevel INFO --ptSelection pt_3.5_-1 --sample DYJetsToLL_M50_LO #SPLIT100
 
 #python step1_select.py  --version v1 --year 2016 --flavour muo --logLevel INFO --ptSelection pt_3.5_-1 --sample QCD_Mu_Pt30to50 #SPLIT100
 #python step1_select.py  --version v1 --year 2016 --flavour ele --logLevel INFO --ptSelection pt_5_-1 --sample QCD_Mu_Pt30to50 #SPLIT100

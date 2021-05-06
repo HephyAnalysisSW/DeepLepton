896ec590f7053610511d21107a8678f56efbf44a
Dietrich Liko <Dietrich.Liko@oeaw.ac.at>
2021-05-06 16:28:38 +0200

    Small change

--- preprocessing/step1_select_dispaced.sh
+++ preprocessing/step1_select_dispaced.sh
@@ -2,4 +2,5 @@
 
 eval `scramv1 runtime -sh`
 
-python step1_select.py  --version v0 --small --year 2017 --flavour muo --logLevel INFO --ptSelection pt_3.5_-1 --displaced --sample CompSUSY
+python step1_select.py  --version v3d --year 2017 --flavour muo --logLevel INFO --ptSelection pt_3.5_-1 --displaced --sample CompSUSY
+python step1_select.py  --version v3d --year 2017 --flavour ele --logLevel INFO --ptSelection pt_3.5_-1 --displaced --sample CompSUSY

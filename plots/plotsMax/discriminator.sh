2a39cddb3d69f604b77cf0a4b516316d8570083d
Maximilian Moser <max.moser1@gmail.com>
2021-06-24 15:49:41 +0200

    discriminator fixes

--- plots/plotsMax/discriminator.sh
+++ plots/plotsMax/discriminator.sh
@@ -1,4 +1,4 @@
 python discriminator.py --directory /scratch-cbe/users/maximilian.moser/DeepLepton/predicted/v3/muo/pt_3.5_-1/TT_pow --mode Top
 python discriminator.py --directory /scratch-cbe/users/maximilian.moser/DeepLepton/predicted/v3/muo/pt_3.5_-1/ --mode DYvsQCD
-python discriminator.py --directory /scratch-cbe/users/maximilian.moser/DeepLepton/predicted/v3/ele/pt_5_-1/ --mode Top
+python discriminator.py --directory /scratch-cbe/users/maximilian.moser/DeepLepton/predicted/v3/ele/pt_5_-1/TT_pow --mode Top
 python discriminator.py --directory /scratch-cbe/users/maximilian.moser/DeepLepton/predicted/v3/ele/pt_5_-1/ --mode DYvsQCD

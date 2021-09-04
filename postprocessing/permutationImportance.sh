953f02d24dfcd28d83cd7f20f43f1118fab1f578
Maximilian Moser <max.moser1@gmail.com>
2021-09-04 14:34:15 +0200

    permutation

---
+++ postprocessing/permutationImportance.sh
@@ -0,0 +1,15 @@
+# 2016 #
+# muo
+
+
+#python permutationImportance.py --small  --year 2016 --flavour muo --versionName v3 --logLevel INFO --ptSelection pt_3.5_-1 --sample TT_pow --modelPath /scratch-cbe/users/maximilian.moser/DeepLepton/Train_muo_2016_Top_v3/training_20/KERAS_check_best_model.h5 
+python permutationImportance.py --small  --year 2016 --flavour muo --versionName v3 --logLevel INFO --ptSelection pt_3.5_-1 --sample DYJetsToLL_M50_LO --modelPath /scratch-cbe/users/maximilian.moser/DeepLepton/Train_muo_2016_DYvsQCD_v3/training_20/KERAS_check_best_model.h5 
+
+
+
+
+
+
+
+
+

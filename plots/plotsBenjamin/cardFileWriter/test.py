51c2d5a6d402748316e9de5a121b13aebf31239c
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2021-11-10 15:24:19 +0100

    test limit script

--- plots/plotsBenjamin/cardFileWriter/test.py
+++ plots/plotsBenjamin/cardFileWriter/test.py
@@ -4,19 +4,27 @@ c = cardFileWriter.cardFileWriter()
 
 cardFileNameTxt = "./card_file.txt"
 
-processes = [ "signal", "tt" ]
+processes = [ "tt" ]
+
+
+c.addUncertainty( "lumi", "lnN" )
 
 c.addBin("SR", processes )
-
-
 c.specifyExpectation( "SR", "signal", 100. )
 c.specifyExpectation( "SR", "tt", 5000. )
-c.addUncertainty( "lumi", "lnN" )
 c.specifyObservation( "SR", 5000)
 
-for p in processes:
+c.addBin("SR2", processes )
+c.specifyExpectation( "SR2", "signal", 50. )
+c.specifyExpectation( "SR2", "tt", 25. )
+c.specifyObservation( "SR2", 25)
+
+for p in processes+["signal"]:
     c.specifyUncertainty( "lumi", "SR", p, 1.036 )
+    c.specifyUncertainty( "lumi", "SR2", p, 1.036 )
 
 c.writeToFile( cardFileNameTxt )
+
+c.calcLimit()
 
 
